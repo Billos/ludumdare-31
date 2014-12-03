af58b5bfa4e349b9dba49acdcf8968098bdb09b5
Goubin Brendan <goubinb1@niksula.hut.fi>
2014-12-03 17:49:58 +0200

    first commit

---
+++ Alpaga/desktop/src/fr/ld/alpaga/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package fr.ld.alpaga.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import fr.ld.alpaga.Launcher;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Launcher(), config);
+	}
+}
