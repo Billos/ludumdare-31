77a6d1f4a71d8fe2e86c9bb2f8ccdf24a9322e7f
Goubin Brendan <goubinb1@niksula.hut.fi>
2014-12-06 15:56:55 +0200

    ongoing for the button

--- core/src/com/ld/alpaga/screen/GameScreen.java
+++ core/src/com/ld/alpaga/screen/GameScreen.java
@@ -7,10 +7,16 @@ import com.badlogic.gdx.graphics.GL20;
 public class GameScreen implements Screen {
 
 	
+	private Screen launcher;
+	
+	public GameScreen(Screen launcher) {
+		this.launcher = launcher;
+	}
+	
 	@Override
 	public void render(float delta) {
 		
-		Gdx.gl.glClearColor(1, 255, 0, 1);
+		Gdx.gl.glClearColor(255, 0, 0, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 	
 	}
--- core/src/com/ld/alpaga/screen/MenuScreen.java
+++ core/src/com/ld/alpaga/screen/MenuScreen.java
@@ -3,14 +3,33 @@ package com.ld.alpaga.screen;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton.TextButtonStyle;
+import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
 import com.ld.alpaga.Launcher;
+import com.ld.alpaga.util.Dictionary;
 
 public class MenuScreen implements Screen {
 
 	private Launcher launcher;
+	private TextButton gameButton;
+	private BitmapFont font;
 	
 	public MenuScreen(Launcher launcher) {
 		this.launcher = launcher;
+		
+    	TextureRegionDrawable drawU = new TextureRegionDrawable(new TextureRegion(new Texture(Gdx.files.internal("style/menu/button/up.png"))));
+    	TextureRegionDrawable drawD = new TextureRegionDrawable(new TextureRegion(new Texture(Gdx.files.internal("style/menu/button/down.png"))));
+    	TextureRegionDrawable drawC = new TextureRegionDrawable(new TextureRegion(new Texture(Gdx.files.internal("style/menu/button/checked.png"))));
+    	
+    	this.font = new BitmapFont(Gdx.files.internal("style/menu/button/font.fnt"));
+    	
+    	TextButtonStyle style = new TextButtonStyle(drawU, drawD, drawC, font);
+		this.gameButton = new TextButton(Dictionary.buttonMenuLaunch, style);
+		
 	}
 	
 	
@@ -19,7 +38,9 @@ public class MenuScreen implements Screen {
 
 		Gdx.gl.glClearColor(0, 0, 255, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-	
+		
+		this.gameButton.setPosition(100, 100);
+		
 	}
 
 	@Override
@@ -56,6 +77,11 @@ public class MenuScreen implements Screen {
 	public void dispose() {
 		// TODO Auto-generated method stub
 		
+	}
+	
+	
+	private void launchGame(){
+		launcher.setScreen(new GameScreen(this));
 	}
 
 }
