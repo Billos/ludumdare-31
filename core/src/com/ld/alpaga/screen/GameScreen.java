adda7c8ac5f451e07846ed966710bff730886a44
Goubin Brendan <goubinb1@niksula.hut.fi>
2014-12-06 14:38:36 +0200

    gamescreen

--- core/src/com/ld/alpaga/screen/GameScreen.java
+++ core/src/com/ld/alpaga/screen/GameScreen.java
@@ -3,22 +3,15 @@ package com.ld.alpaga.screen;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
-import com.ld.alpaga.Launcher;
 
-public class MenuScreen implements Screen {
+public class GameScreen implements Screen {
 
-	private Launcher launcher;
-	
-	public MenuScreen(Launcher launcher) {
-		this.launcher = launcher;
-	}
-	
 	
 	@Override
 	public void render(float delta) {
-
-		Gdx.gl.glClearColor(0, 0, 255, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		
+		Gdx.gl.glClearColor(1, 255, 0, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 	
 	}
 
